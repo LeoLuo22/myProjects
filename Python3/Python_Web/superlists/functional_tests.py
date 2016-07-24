d43879f96440ea1916f6f09f4f63bf30dff04c48
Hao Luo <Hao Luo>
2016-07-24 16:35:07 +0800

    As the title

--- Python3/Python_Web/superlists/functional_tests.py
+++ Python3/Python_Web/superlists/functional_tests.py
@@ -4,5 +4,27 @@ __author__ = "Leo Luo"
 from selenium import webdriver
 
 brower = webdriver.Firefox()
+
+#Someone heard of a cool online TODO app
+#He went to the webpage of this app
 brower.get("http://127.0.1.1:8080")
-assert "Django" in brower.title
+
+#He noticed that the title and header of this page include "TODO"
+assert "To_Do" in brower.title
+
+#The app invites him to input a to-do
+#He input "Buy peacock feathers" in a text
+#His hobbies are using the flys to fish
+#He pressd Enter and the page was refreshed
+#The to-do list showed "1: Buy peacock feathers"
+
+#Another text frame shows, and it can input other to-dos
+#He inputs "Use peacock feathers to make a fly"
+#Refreshed again,His list shows the to-dos
+#He wants to know weather the website would remember his list
+#He sees the website generate a unique URL
+#And some instructions shows
+#He viewd the URL and finds that the list exists
+#He is satisfacted,go to bed
+
+brower.quit()
